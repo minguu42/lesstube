72338dc7b728f5b0491899068113cc171449bead
minguu <minguu42@gmail.com>
2021-09-02 18:32:14 +0900

    SeachResponse, SeachListResponse 型と関連する関数の追加

--- src/models/video.ts
+++ src/models/video.ts
@@ -40,6 +40,46 @@ export const isVideosResponse = (arg: unknown): arg is VideosResponse => {
   return Array.isArray(vs?.items) && vs?.items.every(isVideoResponse);
 };
 
+export type SearchResponse = {
+  id: {
+    videoId: string;
+  };
+  snippet: {
+    publishedAt: string;
+    title: string;
+    thumbnails: {
+      medium: {
+        url: string;
+      };
+    };
+    channelTitle: string;
+  };
+};
+
+const isSearchResponse = (arg: unknown): arg is SearchResponse => {
+  const s = arg as SearchResponse;
+
+  return (
+    typeof s?.id?.videoId === "string" &&
+    typeof s?.snippet?.publishedAt === "string" &&
+    typeof s?.snippet?.title === "string" &&
+    typeof s?.snippet?.thumbnails?.medium?.url === "string" &&
+    typeof s?.snippet?.channelTitle === "string"
+  );
+};
+
+export type SearchListResponse = {
+  items: SearchResponse[];
+};
+
+export const isSearchListResponse = (
+  arg: unknown
+): arg is SearchListResponse => {
+  const sr = arg as SearchListResponse;
+
+  return Array.isArray(sr?.items) && sr?.items.every(isSearchResponse);
+};
+
 export type Video = {
   id: string;
   title: string;
@@ -62,6 +102,23 @@ const newVideo = (videoResp: VideoResponse): Video => {
 
 export const newVideos = (videosResp: VideosResponse): Video[] => {
   return videosResp.items.map((item) => newVideo(item));
+};
+
+const newVideoFromSearch = (searchResp: SearchResponse): Video => {
+  return {
+    id: searchResp.id.videoId,
+    title: searchResp.snippet.title,
+    thumbnailURL: searchResp.snippet.thumbnails.medium.url,
+    channelTitle: searchResp.snippet.channelTitle,
+    viewCount: 0,
+    publishedAt: new Date(searchResp.snippet.publishedAt),
+  };
+};
+
+export const newVideosFromSearch = (
+  searchList: SearchListResponse
+): Video[] => {
+  return searchList.items.map((item) => newVideoFromSearch(item));
 };
 
 export const videosState = atom<Video[]>({
