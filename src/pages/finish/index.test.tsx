8edf3dd4218d838f8daa01d61e72665d348c13cb
minguu <minguu42@gmail.com>
2021-09-14 11:32:54 +0900

    /finish ページのテストの修正
    他のテストと一貫性を持たせるためグローバルスコープの beforeEach でレンダリングを行うように変更した.
    テストスイート名, テストケース名を日本語に変更した.

--- src/pages/finish/index.test.tsx
+++ src/pages/finish/index.test.tsx
@@ -1,20 +1,20 @@
 import { screen } from "@testing-library/react";
 import { getPage } from "next-page-tester";
 
-describe("/finish page display", () => {
-  beforeEach(async () => {
-    const { render } = await getPage({ route: "/finish" });
-    render();
-  });
+beforeEach(async () => {
+  const { render } = await getPage({ route: "/finish" });
+  render();
+});
 
-  test("display message", () => {
+describe("Finish ページの表示に関するテスト", () => {
+  test("メッセージの表示", () => {
     expect(screen.getByText(/Thank you for watching./i)).toBeInTheDocument();
     expect(screen.getByText(/Have a nice day!/i)).toBeInTheDocument();
   });
 
-  test("display image", () => {
+  test("画像の表示", () => {
     expect(
-      screen.getAllByRole("img", { name: "Goodbye Image" })[1]
+      screen.getAllByRole("img", { name: "Goodbye Image" })[0]
     ).toBeInTheDocument();
   });
 });
